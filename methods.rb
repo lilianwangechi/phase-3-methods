f4db55f296ed40caf9590bd6e70edf15ef2f2213
lilian <lilian.wangechi@student.moringaschool.com>
2022-10-17 01:26:40 +0300

    done

--- methods.rb
+++ methods.rb
@@ -1,1 +1,36 @@
 # Your code here!
+def greet_programmer
+  puts "Hello, programmer!"
+
+end
+
+def greet(name)
+  puts "Hello, #{name}!"
+end
+
+greet ("Naureen")
+greet ("Jimmy")
+
+def greet_with_default(name = "programmer")
+  puts "Hello, #{name}!"
+end
+
+greet_with_default 
+greet_with_default "Naureen"
+
+#add
+def add(num1, num2)
+  return num1 + num2
+end
+sum1 = add(3, 4)
+sum2 = add(3, 4)
+
+#halve
+def halve(nam)
+  if nam.class != Integer
+    return nil
+  end
+
+  return nam/2
+end
+halve (6)
